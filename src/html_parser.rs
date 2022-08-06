f58429d38f309da901430e9cd7c359f58eb3fc29
kz_morita <kz.morit@gmail.com>
2022-08-06 14:07:42 +0900

    add attribute parser

--- src/html_parser.rs
+++ src/html_parser.rs
@@ -1,4 +1,35 @@
+use combine::parser::char::char;
+use combine::parser::char::letter;
+use combine::parser::char::newline;
+use combine::parser::char::space;
+use combine::parser::Parser;
+#[allow(unused_imports)]
+use combine::EasyParser;
+use combine::ParseError;
+use combine::Stream;
+use combine::{between, many, many1, satisfy};
+
+fn attribute<Input>() -> impl Parser<Input, Output = (String, String)>
+where
+    Input: Stream<Token = char>,
+    Input::Error: ParseError<Input::Token, Input::Range, Input::Position>,
+{
+    (
+        many1::<String, _, _>(letter()), // read attribute name any letters
+        many::<String, _, _>(space().or(newline())), // skip read space and newline
+        char('='),                       // read '='
+        many::<String, _, _>(space().or(newline())), // skip read space and newline
+        between(
+            char('"'),
+            char('"'),
+            many1::<String, _, _>(satisfy(|c: char| c != '"')),
+        ), // read character expecting quates
+    )
+        .map(|v| (v.0, v.4))
+}
+
 mod test {
+    #[allow(unused_imports)]
     use super::*;
 
     #[test]
